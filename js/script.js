38a5ea2d7c86feacd8dad94d5d1e3a079f018a07
Vrajpal Jhala <jhalavrajpal@gmail.com>
2020-02-17 18:27:30 +0530

    Added update, search feature and dynamic range filter

--- js/script.js
+++ js/script.js
@@ -15,6 +15,10 @@ var priceFilterValue = document.getElementById("priceFilterValue");
 var categoryResidential = document.getElementById("categoryResidential");
 var categoryCommercial = document.getElementById("categoryCommercial");
 
+var searchBar = document.getElementById("searchProject");
+
+var areaLowerRange = 0, areaUpperRange = 0, ageLowerRange = 0, ageUpperRange = 0, priceLowerRange = 0, priceUpperRange = 0;
+
 function showFilterBarValues() {
     areaFilterValue.innerHTML = "Value: " + areaFilterBar.value;
     ageFilterValue.innerHTML = "Value: " + ageFilterBar.value;
@@ -24,7 +28,76 @@ function showFilterBarValues() {
 document.addEventListener("DOMContentLoaded", function () {
     projects = JSON.parse(data).projects;
     applyFilters();
+
+    document.getElementById("projectTable").onclick = function (e) {
+        var column = e.target;
+        if (e.target.tagName == "TD") {
+            var row = column.parentNode;
+            var rowNo = row.querySelector("span").getAttribute("data-row");
+            var columnNo = Array.from(row.querySelectorAll("td")).findIndex(column => column == e.target);
+
+            currValue = column.innerHTML;
+            if (columnNo == 1) {
+                currValue = currValue.slice(0, -3);
+            }
+
+            if (columnNo == 2) {
+                currValue = currValue.slice(0, -4);
+            }
+
+            if (columnNo == 3) {
+                currValue = currValue.slice(0, -3);
+            }
+
+            if (columnNo == 4) {
+                column.innerHTML = "<select name='newValue' id='newValue' class='form-control' onchange='updateProject(this, " + rowNo
+                    + ")' onblur='updateProject(this, " + rowNo + ")' data-col='" + columnNo + "'><option value='Residential'>Residential</option>" +
+                    "<option value='Commercial'>Commercial</option></select>"
+            } else {
+                column.innerHTML = "<input type='text' class='form-control' id='newValue' name='newValue' onchange='updateProject(this, "
+                    + rowNo + ")' onblur='undoUpdate(this, " + rowNo + ")' value='" + currValue + "' data-col='" + columnNo + "'>";
+            }
+
+            document.getElementById("newValue").focus();
+        }
+    };
 });
+
+function setFilterBarRange(project) {
+    ageLowerRange = ageLowerRange == 0 ? Number(project.projectAge) : ageLowerRange;
+    areaLowerRange = areaLowerRange == 0 ? Number(project.projectArea) : areaLowerRange;
+    priceLowerRange = priceLowerRange == 0 ? Number(project.projectPrice) : priceLowerRange;
+
+    if (project.projectAge < ageLowerRange) {
+        ageLowerRange = Number(project.projectAge);
+        ageFilterBar.setAttribute("min", ageLowerRange);
+    }
+
+    if (project.projectAge > ageUpperRange) {
+        ageUpperRange = Number(project.projectAge);
+        ageFilterBar.setAttribute("max", ageUpperRange);
+    }
+
+    if (project.projectArea < areaLowerRange) {
+        areaLowerRange = Number(project.projectArea);
+        areaFilterBar.setAttribute("min", areaLowerRange);
+    }
+
+    if (project.projectArea > areaUpperRange) {
+        areaUpperRange = Number(project.projectArea);
+        areaFilterBar.setAttribute("max", areaUpperRange);
+    }
+
+    if (project.projectPrice < priceLowerRange) {
+        priceLowerPrice = Number(project.projectPrice);
+        priceFilterBar.setAttribute("min", priceLowerRange);
+    }
+
+    if (project.projectPrice > priceUpperRange) {
+        priceUpperRange = Number(project.projectPrice);
+        priceFilterBar.setAttribute("max", priceUpperRange);
+    }
+}
 
 function addCellData(row, colData) {
     var newCell = row.insertCell();
@@ -34,16 +107,21 @@ function addCellData(row, colData) {
 function applyFilters() {
     var rowNo = 0;
     var noResults = true;
-    showFilterBarValues();
     tableBody.innerHTML = "";
 
     var onlyResidential = categoryResidential.checked;
     var onlyCommercial = categoryCommercial.checked;
     var both = onlyResidential == onlyCommercial;
 
+    var searchPattern = searchBar.value == "" ? "[\s\S]" : searchBar.value;
+
     for (project of projects) {
+        setFilterBarRange(project);
+
         if (Number(areaFilterBar.value) >= project.projectArea && Number(ageFilterBar.value) >= project.projectAge && Number(priceFilterBar.value) >= project.projectPrice &&
-            (project.projectCategory == (onlyResidential || both ? "Residential" : "") || project.projectCategory == (onlyCommercial || both ? "Commercial" : ""))) {
+            (project.projectCategory == (onlyResidential || both ? "Residential" : "") || project.projectCategory == (onlyCommercial || both ? "Commercial" : ""))
+            && project.projectName.search(searchPattern)) {
+
             noResults = false;
             var newRow = tableBody.insertRow();
 
@@ -52,15 +130,19 @@ function applyFilters() {
             addCellData(newRow, project.projectArea + "SqFt");
             addCellData(newRow, project.projectAge + "Yrs");
             addCellData(newRow, project.projectCategory);
-            addCellData(newRow, "<span onclick='removeProject(this)' data-row='" + (rowNo++) + "'><i class='fas fa-times fa-2x text-danger'></i></span>");
+            addCellData(newRow, "<span onclick='removeProject(this)' data-row='" + rowNo + "'><i class='fas fa-times fa-2x text-danger'></i></span>");
         }
+
+        rowNo++;
     }
+
+    showFilterBarValues();
 
     var newRow = tableBody.insertRow();
     addCellData(newRow, "<input type='text' class='form-control' name='newProjectName' id='newProjectName'>");
-    addCellData(newRow, "<input type='number' class='form-control' name='newProjectPrice' min='1' max='95' id='newProjectPrice'>");
-    addCellData(newRow, "<input type='number' class='form-control' name='newProjectArea' min='1' max='200' id='newProjectArea'>");
-    addCellData(newRow, "<input type='number' class='form-control' name='newProjectAge' min='1' max='50' id='newProjectAge'>");
+    addCellData(newRow, "<input type='number' class='form-control' name='newProjectPrice' id='newProjectPrice'>");
+    addCellData(newRow, "<input type='number' class='form-control' name='newProjectArea' id='newProjectArea'>");
+    addCellData(newRow, "<input type='number' class='form-control' name='newProjectAge' id='newProjectAge'>");
     addCellData(newRow, "<select class='form-control' name='newProjectCategory' id='newProjectCategory'>" +
         "<option value='Residential'>Residential</option><option value='Commercial'>Commercial</option></select>");
     addCellData(newRow, "<span onclick='addProject()'><i class='fas fa-plus fa-2x text-success'></i></span>");
@@ -85,19 +167,19 @@ function validateProject(project) {
         return false;
     }
 
-    if (project.projectPrice == "" || project.projectPrice > 95) {
+    if (project.projectPrice == "") {
         $('#alertModal').find('.modal-body').html("Enter valid price");
         $('#alertModal').modal();
         return false;
     }
 
-    if (project.projectArea == "" || project.projectArea > 200) {
+    if (project.projectArea == "") {
         $('#alertModal').find('.modal-body').html("Enter valid area");
         $('#alertModal').modal();
         return false;
     }
 
-    if (project.projectAge == "" || project.projectAge > 10) {
+    if (project.projectAge == "") {
         $('#alertModal').find('.modal-body').html("Enter valid age");
         $('#alertModal').modal();
         return false;
@@ -115,9 +197,9 @@ function addProject() {
 
     var project = {
         projectName: newProjectName,
-        projectPrice: Number(newProjectPrice),
-        projectArea: Number(newProjectArea),
-        projectAge: Number(newProjectAge),
+        projectPrice: newProjectPrice,
+        projectArea: newProjectArea,
+        projectAge: newProjectAge,
         projectCategory: newProjectCategory
     };
 
@@ -125,4 +207,35 @@ function addProject() {
         projects.push(project);
         applyFilters();
     }
+}
+
+function updateProject(obj) {
+    var rowNo = Number(obj.parentNode.parentNode.querySelector("span").getAttribute("data-row"));
+    var colNo = Number(obj.getAttribute("data-col"));
+
+    if (colNo == 0)
+        projects[rowNo].projectName = obj.value;
+
+    if (colNo == 1)
+        projects[rowNo].projectPrice = obj.value;
+
+    if (colNo == 2)
+        projects[rowNo].projectArea = obj.value;
+
+    if (colNo == 3)
+        projects[rowNo].projectAge = obj.value;
+
+    if (colNo == 4)
+        projects[rowNo].projectCategory = obj.value;
+
+    applyFilters();
+}
+
+function undoUpdate(obj) {
+    applyFilters();
+}
+
+function clearSearch() {
+    searchBar.value = "";
+    applyFilters();
 }
